f212d5a9e95720694a0f6be4a28255a2ad9079ba
utkarshpathak14 <148760143+utkarshpathak14@users.noreply.github.com>
2025-01-07 13:09:09 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HomePage />
+      
+    </>
+  );
+}
+
+export default App;
